94dacd860210c48069203d278796d33d6248cd37
Saul Rodriguez <saulrodriguez221299@gmail.com>
2026-04-03 17:10:26 -0600

    initial commit

---
+++ src/GifsApp.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+import { GifList } from "./gifs/components/GifList"
+import { PreviousSearches } from "./gifs/components/PreviousSearches"
+import { mockGifs } from "./mock-data/gifs.mock"
+import { CustomHeader } from "./shared/components/CustomHeader"
+import { SearchBar } from "./shared/components/SearchBar"
+import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action"
+
+
+export const GifsApp = () => {
+    const [previousTerms, setPreviousTerms] = useState(['dragon ball z']);
+    const handleTermClicked = (term: string) => {
+        console.log({term});
+    } 
+    const handleSearch = async (query: string) => {
+        query = query.toLowerCase().trim();
+
+        if(query.length === 0) return;
+
+        if(previousTerms.includes(query)) return;
+        
+        
+        setPreviousTerms((prev) => {
+            const updated = [query, ...prev];
+            return updated.slice(0,8);
+        })
+
+        const gifs = await getGifsByQuery(query);
+    }
+    return (
+        <>
+            {/* Header */}
+            <CustomHeader title="Buscador de gifs" description="Descubre y comparte el Gif perfecto" />
+            {/* Search */}
+            <SearchBar 
+                placeholder="Buscar gifs"
+                onQuery= {handleSearch}
+            />
+            {/* Búsquedas previas */}
+            <PreviousSearches searches={previousTerms} onLabelClicked={handleTermClicked}/>
+            {/* Gifs */}
+            <GifList gifs={mockGifs} />
+        </>
+    )
+}
