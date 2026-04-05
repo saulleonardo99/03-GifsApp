1481ddd469134f3dbd6bb5626a410014c8946945
Saul <saulleonardo99@hotmail.com>
2026-04-05 14:08:42 -0600

    feat: fetch searched gifs from Giphy API

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,14 +1,15 @@
 import { useState } from "react"
 import { GifList } from "./gifs/components/GifList"
 import { PreviousSearches } from "./gifs/components/PreviousSearches"
-import { mockGifs } from "./mock-data/gifs.mock"
 import { CustomHeader } from "./shared/components/CustomHeader"
 import { SearchBar } from "./shared/components/SearchBar"
 import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action"
+import type { Gif } from "./gifs/interfaces/gif.interface"
 
 
 export const GifsApp = () => {
-    const [previousTerms, setPreviousTerms] = useState(['dragon ball z']);
+    const [previousTerms, setPreviousTerms] = useState<string[]>([]);
+    const [gifs, setGifs] = useState<Gif[]>([]);
     const handleTermClicked = (term: string) => {
         console.log({term});
     } 
@@ -26,6 +27,7 @@ export const GifsApp = () => {
         })
 
         const gifs = await getGifsByQuery(query);
+        setGifs([...gifs])
     }
     return (
         <>
@@ -39,7 +41,7 @@ export const GifsApp = () => {
             {/* Búsquedas previas */}
             <PreviousSearches searches={previousTerms} onLabelClicked={handleTermClicked}/>
             {/* Gifs */}
-            <GifList gifs={mockGifs} />
+            <GifList gifs={gifs} />
         </>
     )
 }
