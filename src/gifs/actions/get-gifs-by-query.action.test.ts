5d2c9d1c582004791e420c408d0cedca87fb2d5e
Saul <saulleonardo99@hotmail.com>
2026-04-11 13:00:00 -0600

    test(getGifsQueryAction): create test to handle different type of response
    Using mocks:
    - Correct response
    - Empty respose
    - Error response

--- src/gifs/actions/get-gifs-by-query.action.test.ts
+++ src/gifs/actions/get-gifs-by-query.action.test.ts
@@ -1,12 +1,14 @@
 import AxiosMockAdapter from 'axios-mock-adapter';
-import { describe, expect, test } from "vitest";
+import { beforeEach, describe, expect, test, vi } from "vitest";
 import { getGifsByQuery } from "./get-gifs-by-query.action";
 import { giphyApi } from "../api/giphy.api";
 import {giphySearchResponseMock} from './../../../tests/mocks/giphy.response.data'
 
 describe('getGifsByQuery', ()=> {
-    const mock = new AxiosMockAdapter(giphyApi); //Inicializa un mock de axios con las configuraciones del giphyApi
-
+    let mock = new AxiosMockAdapter(giphyApi); //Inicializa un mock de axios con las configuraciones del giphyApi
+    beforeEach(() => {
+        mock = new AxiosMockAdapter(giphyApi);
+    })
     // test('should return a list of gifs', async () => {
     //     const gifs = await getGifsByQuery('goku');
     //     const[gif1] = gifs;
@@ -30,5 +32,27 @@ describe('getGifsByQuery', ()=> {
             expect(typeof gif.width).toBe('number');
             expect(typeof gif.height).toBe('number');
         } );
+    })
+    test('should return an empty list of gifs if query is empty', async () => {
+        // mock.onGet('/search').reply(200, giphySearchResponseMock);
+        mock.restore();
+        const gifs = await getGifsByQuery('');
+        expect(gifs.length).toBe(0);
+    });
+    test('should handle error when the API returns error', async () => {
+        const consoleErrorSpy = vi
+            .spyOn(console,'error')
+            .mockImplementation(() => {})
+        mock.onGet('/search').reply(400, {
+            data: {
+                message: 'Bad request'
+            }
+        });
+        const gifs = await getGifsByQuery('gifs');
+        expect(gifs.length).toBe(0);
+        expect(consoleErrorSpy).toHaveBeenCalled();
+        expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
+        expect(consoleErrorSpy).toHaveBeenCalledWith(expect.anything());
+        
     })
 })
